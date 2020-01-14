18f3305d9dd601db0c36df4f22f157cd5b85b8f2
Daniel Sachs <dsachs@bhsai.org>
2020-01-14 14:54:22 -0500

    Updates

---
+++ precache-manifest.e485ade981207595d400d3f17f6a145d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca7098c845d46444cc824325960a3a6",
+    "url": "/daily-temps/index.html"
+  },
+  {
+    "revision": "a9dcf0aed255bca8ecec",
+    "url": "/daily-temps/static/css/main.45cfc9bd.chunk.css"
+  },
+  {
+    "revision": "6070d1ab2d2f3034f4b6",
+    "url": "/daily-temps/static/js/2.e04bcb74.chunk.js"
+  },
+  {
+    "revision": "a9dcf0aed255bca8ecec",
+    "url": "/daily-temps/static/js/main.ff7c675c.chunk.js"
+  },
+  {
+    "revision": "ea85a5aca5348b793d73",
+    "url": "/daily-temps/static/js/runtime-main.14042725.js"
+  }
+]);
